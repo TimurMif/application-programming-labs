edd87ef26fbbb71a20e1d685b32320f95a360aec
TimurMif <162575403+TimurMif@users.noreply.github.com>
2024-11-19 10:13:36 +0400

    Delete icrawlerdo.py

--- icrawlerdo.py
+++
@@ -1,7 +0,0 @@
-from icrawler.builtin import GoogleImageCrawler
-
-def icrawler(keyw: str, path_to_dir: str):
-    """Use icrawler to download in file abs path to image in enthernet"""
-    google_crawler = GoogleImageCrawler(storage={'root_dir': path_to_dir})
-    filters = dict(size = "large")
-    google_crawler.crawl(keyword= keyw, filters =filters, max_num=3)
