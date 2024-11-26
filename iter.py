f38e4c2d1c5e4367628f1f5ff07d0b9737c1cdbe
TimurMif <162575403+TimurMif@users.noreply.github.com>
2024-11-26 09:03:01 +0400

    Add files via upload

---
+++ iter.py
@@ -0,0 +1,32 @@
+import os
+
+
+class IteratorInDir:
+    """This class is move into the folder and print abs path of files in folder"""
+    def __init__(self, folder_path):
+        self.filenames = []
+        self.fileabspaths = []
+        self.counter = -1
+        for file_name in os.listdir(folder_path):
+            if os.path.isfile(os.path.join(folder_path, file_name))\
+                and file_name.endswith(('jpg', 'jpeg', 'png')):
+                self.filenames.append(file_name)
+                self.fileabspaths.append(str(folder_path + "/" + file_name))
+    def __iter__(self):
+        return self
+
+    def prev(self):
+        if self.counter > 0:
+            self.counter -= 1
+            return self.fileabspaths[self.counter]
+        else:
+            self.counter = len(self.fileabspaths) - 1
+            return self.fileabspaths[self.counter]
+
+    def __next__(self):
+        self.counter += 1
+        if self.counter < len(self.fileabspaths):
+            return self.fileabspaths[self.counter]
+        else:
+            self.counter = 0
+            return self.fileabspaths[self.counter]
